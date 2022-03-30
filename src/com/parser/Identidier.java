9bac9540703001110470a82ec68656042c226b9c
vlad <vladichek3322@gmail.com>
2022-03-30 12:49:24 +0700

    add logging

--- src/com/parser/Identidier.java
+++
@@ -1,8 +0,0 @@
-package com.parser;
-
-public class Identidier {
-    static boolean isOperation(String name) {
-        return name.equals("+") || name.equals("-") || name.equals("*")
-                || name.equals("/") || name.equals("SQRT") || name.equals("PRINT") || name.equals("POP");
-    }
-}
